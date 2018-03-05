8557e45bf407ea69d64849da035f6ada512748cb
a-yu-relativity <ayu@kcura.com>
2018-03-05 10:41:24 -0600

    Initial commit

---
+++ ListPageSample/ListPageInteraction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using kCura.EventHandler;
+
+namespace ListPageSample
+{
+    [kCura.EventHandler.CustomAttributes.Description("Sample Document object ListPage Event Handler")]
+    [Guid("B6ABE979-BD53-4AE2-9966-4DF5E87FB25F")]
+    public class ListPageInteraction : ListPageInteractionEventHandler
+    {
+        public override Response PopulateScriptBlocks()
+        {
+            Response response = new Response
+            {
+                Success = true,
+                Message = String.Empty
+            };
+
+            string script = ScriptResource.script;
+
+            string block = $"<script type=\"text/javascript\"> {script} </script>";
+            RegisterStartupScriptBlock(new ScriptBlock()
+            {
+                Key = "script.js",
+                Script = block
+            });
+
+            return response;
+        }
+
+        //public override string[] ScriptFileNames => new[] { "script.js" };
+    }
+}
---
+++ ListPageSample/javascript/script.js
@@ -0,0 +1,23 @@
+﻿define(function ()
+{
+    function sampleHandler(api)
+    {
+        // Function that gets called when view/browser changed. Gets data object that has viewId, viewLabel and browser
+        function viewChange(data)
+        {
+            //var viewId = data.viewId;
+            //var browser = data.browser;
+            // if (viewId === 1061130) {
+            //     // do something
+            //     console.log("Switched to extracted text view!")
+            // }
+            console.log("something happened.");
+        }
+
+        return {
+            // Subscribe to the "viewChange" event
+            viewChange: viewChange
+        }
+    }
+    return sampleHandler;
+});
